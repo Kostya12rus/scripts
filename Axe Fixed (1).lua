b87ebca55038f6d708d90166310129c506d0250d
Kostya12rus <mkg97@yandex.ru>
2017-04-18 16:25:35 +0300

    18.04.2017

--- Axe Fixed (1).lua
+++ Axe Fixed (1).lua
@@ -1,11 +1,12 @@
 local Axe ={}
 
-Axe.optionEnable = Menu.AddOption({"Hero Specific", "Axe2"}, "Enabled", "")
-Axe.optionKey = Menu.AddKeyOption({"Hero Specific", "Axe2"}, "ComboKey", Enum.ButtonCode.KEY_F)
-Axe.bkbEnable = Menu.AddOption({"Hero Specific", "Axe2"}, "Use BKB", "")
-Axe.pipeEnable = Menu.AddOption({"Hero Specific", "Axe2"}, "Use Pipe", "")
-Axe.staffEnable = Menu.AddOption({"Hero Specific", "Axe2"}, "Use Force Staff[BETA]", "")
-Axe.battlehungerEnable = Menu.AddOption({"Hero Specific", "Axe2"}, "Use Battlehunger", "")
+Axe.optionEnable = Menu.AddOption({"Hero Specific", "Axe3"}, "Enabled", "")
+Axe.optionKey = Menu.AddKeyOption({"Hero Specific", "Axe3"}, "ComboKey", Enum.ButtonCode.KEY_F)
+Axe.bkbEnable = Menu.AddOption({"Hero Specific", "Axe3"}, "Use BKB", "")
+Axe.mjollnirEnable = Menu.AddOption({"Hero Specific", "Axe3"}, "Use Mjollnir", "")
+Axe.pipeEnable = Menu.AddOption({"Hero Specific", "Axe3"}, "Use Pipe", "")
+Axe.staffEnable = Menu.AddOption({"Hero Specific", "Axe3"}, "Use Force Staff[BETA]", "")
+Axe.battlehungerEnable = Menu.AddOption({"Hero Specific", "Axe3"}, "Use Battlehunger", "")
 Axe.Hero = nil
 
 function Axe.OnUpdate()
@@ -28,6 +29,7 @@ local stan = NPC.GetAbilityByIndex(myHero,0)
 local bh = NPC.GetAbilityByIndex(myHero,1)
 
 local shiva = NPC.GetItem(myHero, "item_shivas_guard", true)
+local mjollnir = NPC.GetItem(myHero, "item_mjollnir", true)
 local lotus = NPC.GetItem(myHero,"item_lotus_orb", true)
 local bm = NPC.GetItem(myHero,"item_blade_mail", true)
 local medallion = NPC.GetItem(myHero, "item_medallion_of_courage", true)
@@ -42,6 +44,7 @@ local mousePos = Input.GetWorldCursorPos()
  --blink
  if staff and Ability.IsCastable(staff, myMana) and NPC.IsPositionInRange(myHero, NPC.GetAbsOrigin(hero),1650,0)  and not NPC.IsPositionInRange(myHero, NPC.GetAbsOrigin(hero),1000,0) and Menu.IsEnabled(Axe.staffEnable) then Ability.CastTarget(staff,myHero) return end
  if lotus and Ability.IsCastable(lotus,myMana) and NPC.IsPositionInRange(myHero, NPC.GetAbsOrigin(hero),1200,0) then Ability.CastTarget(lotus,myHero) return end
+ if mjollnir and Ability.IsCastable(mjollnir,myMana) and NPC.IsPositionInRange(myHero, NPC.GetAbsOrigin(hero),1200,0) then Ability.CastTarget(mjollnir,myHero) return end
  if blink and Ability.IsCastable(blink, myMana) and NPC.IsPositionInRange(myHero, NPC.GetAbsOrigin(hero),1100,0) then Ability.CastPosition(blink,heroPos) return end 
  if Ability.IsCastable(stan, myMana) and hero ~=nil and NPC.IsPositionInRange(myHero,NPC.GetAbsOrigin(hero), 0 , 300)  then Ability.CastNoTarget(stan) return end
  if bm and Ability.IsCastable(bm, myMana) and NPC.IsPositionInRange(myHero, NPC.GetAbsOrigin(hero),350,0) then Ability.CastNoTarget(bm) return end
