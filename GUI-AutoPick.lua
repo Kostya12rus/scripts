fdc4d0a8d60502f6315074438607cb1510584ddd
Kostya12rus <mkg97@yandex.ru>
2017-06-20 23:41:52 +0300

    20.06.2017

--- GUI-AutoPick.lua
+++ GUI-AutoPick.lua
@@ -12,6 +12,14 @@ AP.Locale = {
 	["select"] = {
 		["english"] = "Select Hero",
 		["russian"] = "Выберите героя"
+	},
+	["write"] = {
+		["english"] = "Write message on start",
+		["russian"] = "Написать сообщение в начале"
+	},
+	["chat_message"] = {
+		["english"] = "Message",
+		["russian"] = "Сообщение"
 	}
 }
 
@@ -20,7 +28,12 @@ function AP.OnDraw()
 	
 	if not GUI.Exist(AP.Identity) then
 		GUI.Initialize(AP.Identity, GUI.Category.General, AP.Locale["name"], AP.Locale["desc"], "paroxysm")
-		GUI.AddMenuItem(AP.Identity, AP.Identity .. "pickedHero", AP.Locale["select"], GUI.MenuType.ImageBox, 1, GUI.HeroesList, GUI.HeroesIconPath, "npc_dota_hero_")
+		local templist = GUI.HeroesList
+		templist["roshan"] = "zRandom"
+		GUI.AddMenuItem(AP.Identity, AP.Identity .. "pickedHero", AP.Locale["select"], GUI.MenuType.ImageBox, 1, templist, GUI.HeroesIconPath, "npc_dota_hero_")
+		GUI.Subscribe(AP.Identity, GUI.GameStates.OnLobbyLoad, AP.WriteMessage)
+		GUI.AddMenuItem(AP.Identity, AP.Identity .. "write_message", AP.Locale["write"], GUI.MenuType.CheckBox, 0)
+		GUI.AddMenuItem(AP.Identity, AP.Identity .. "message", AP.Locale["chat_message"], GUI.MenuType.TextBox, "mid")
 	end
 	
 	if	GameRules.GetGameState() == 2
@@ -29,8 +42,18 @@ function AP.OnDraw()
 		if GUI.Get(AP.Identity .. "pickedHero") == nil then return end
 	
 		for k,v in pairs(GUI.Get(AP.Identity .. "pickedHero", 1)) do
-			Engine.ExecuteCommand("dota_select_hero " .. v)
+			if v == "roshan" then
+				Engine.ExecuteCommand("dota_select_hero random")
+			else
+				Engine.ExecuteCommand("dota_select_hero " .. v)
+			end
 		return end
+	end
+end
+
+function AP.WriteMessage(o, w)
+	if GUI.IsEnabled(AP.Identity .. "write_message") and GUI.IsEnabled(AP.Identity) then
+		Engine.ExecuteCommand("say_team " .. GUI.Get(AP.Identity .. "message"))
 	end
 end
 
