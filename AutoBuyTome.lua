b52c6dd77ec637f70a4fe322f99c3c0d8bb380c6
Kostya12rus <mkg97@yandex.ru>
2018-06-24 20:47:56 +0300

    24.06.2018

--- AutoBuyTome.lua
+++ AutoBuyTome.lua
@@ -1,23 +1,27 @@
-local AutoBuyTome = {}
+local AutoBuyBock = {}
 
-AutoBuyTome.Enable = Menu.AddOption({ "Utility" }, "Auto Buy Tome of Knowledge", "Buy every 10 minuts Tome of Knowledge if you have 150 gold")
+AutoBuyBock.Enable = Menu.AddOption({ "Kostya12rus"}, "Auto Buy Tome of Knowledge", "Buy every 10 minuts Tome of Knowledge if you have 150 gold")
 
-AutoBuyTome.Delay = 0
-function AutoBuyTome.OnUpdate()
-	if not Menu.IsEnabled(AutoBuyTome.Enable) then
-		return
-	end
-
-	local myHero = Heroes.GetLocal()
-
-	if not myHero then
-		return
-	end
-
-	if (GameRules.GetGameTime() - GameRules.GetGameStartTime()) / 60 % 10 < 0.01 and GameRules.GetGameTime() > AutoBuyTome.Delay then
-		Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, 257, Vector(0, 0, 0), 257, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, myHero, false, true)
-		AutoBuyTome.Delay = GameRules.GetGameTime() + 0.2
+function AutoBuyBock.OnUpdate()
+	if not Menu.IsEnabled(AutoBuyBock.Enable) then return end
+ 	local myHero = Heroes.GetLocal()
+	if not myHero then return end
+	if GameRules.GetGameTime() > AutoBuyBock.Delay and not NPC.HasItem(myHero, "item_tome_of_knowledge") then
+		local realtime = (GameRules.GetGameTime() - GameRules.GetGameStartTime()) / 60 % 10
+		if (realtime > 0 and realtime < 0.2) then
+			Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, 257, Vector(0, 0, 0), 257, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, myHero, false, true)
+		end
+		AutoBuyBock.Delay = GameRules.GetGameTime() + 1
 	end
 end
 
-return AutoBuyTome
+function AutoBuyBock.init()
+	AutoBuyBock.Delay = 0
+end
+
+function AutoBuyBock.OnGameStart()
+	AutoBuyBock.init()
+end
+AutoBuyBock.init()
+
+return AutoBuyBock
