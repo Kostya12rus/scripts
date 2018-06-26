40b1b66cf0d790e4cacddd2ce731d436a360354b
Kostya12rus <mkg97@yandex.ru>
2018-06-27 00:08:38 +0300

    27.06.2018

--- AutoBuyTome.lua
+++ AutoBuyTome.lua
@@ -6,9 +6,9 @@ function AutoBuyBock.OnUpdate()
 	if not Menu.IsEnabled(AutoBuyBock.Enable) then return end
  	local myHero = Heroes.GetLocal()
 	if not myHero then return end
-	if GameRules.GetGameTime() > AutoBuyBock.Delay and not NPC.HasItem(myHero, "item_tome_of_knowledge") then
+	if GameRules.GetGameTime() > AutoBuyBock.Delay then
 		local realtime = (GameRules.GetGameTime() - GameRules.GetGameStartTime()) / 60 % 10
-		if (realtime > 0 and realtime < 0.2) then
+		if realtime > 0 and realtime < 0.02 then
 			Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_PURCHASE_ITEM, 257, Vector(0, 0, 0), 257, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_PASSED_UNIT_ONLY, myHero, false, true)
 		end
 		AutoBuyBock.Delay = GameRules.GetGameTime() + 1
@@ -18,10 +18,11 @@ end
 function AutoBuyBock.init()
 	AutoBuyBock.Delay = 0
 end
-
+function AutoBuyBock.OnGameEnd()
+	AutoBuyBock.init()
+end
 function AutoBuyBock.OnGameStart()
 	AutoBuyBock.init()
 end
 AutoBuyBock.init()
-
 return AutoBuyBock
