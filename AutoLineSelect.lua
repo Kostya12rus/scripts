10cc4c7ee62f96a07296ef41c3d89375bc456e39
Kostya12rus <mkg97@yandex.ru>
2018-09-19 22:21:27 +0300

    19.09.2018

--- AutoLineSelect.lua
+++ AutoLineSelect.lua
@@ -8,14 +8,19 @@ LineSelect.Locale	= {
 		["chinese"] = "自动线选择"
 	},
 	["desc"] = {
-		["english"] = "You'll be the first who choose this line. v0.1",
-		["russian"] = "Вы будете первым кто выберет заданую линию. v0.1",
-		["chinese"] = "你会是第一个谁选择这条线。 v0.1"
+		["english"] = "You'll be the first who choose this line. v0.2",
+		["russian"] = "Вы будете первым кто выберет заданую линию. v0.2",
+		["chinese"] = "你会是第一个谁选择这条线。 v0.2"
 	},
 	["select"] = {
 		["english"] = "Select Line",
 		["russian"] = "Выберите линию",
 		["chinese"] = "选择行"
+	},
+	["select_hero"] = {
+		["english"] = "Select Hero",
+		["russian"] = "Выберите героя",
+		["chinese"] = "选择英雄"
 	},
 	["line_select"] = {
 		["english"] = {
@@ -46,17 +51,17 @@ LineSelect.Locale	= {
 }
 
 function LineSelect.OnDraw()
-	if GUI == nil then return end
+	if GUI == nil or GUIDB == nil then return end
 
 	if not GUI.Exist(LineSelect.Identity) then
-	
+		local templist = GUIDB.Heroes
+
 		GUI.Initialize(LineSelect.Identity, GUI.Category.General, LineSelect.Locale["name"], LineSelect.Locale["desc"], "Zerling14")
+		GUI.AddMenuItem(LineSelect.Identity, LineSelect.Identity .. "pickedHero", LineSelect.Locale["select_hero"], GUI.MenuType.ImageBox, 1, templist, GUIDB.HeroPath)
 		GUI.AddMenuItem(LineSelect.Identity, LineSelect.Identity .. "line_select_option", LineSelect.Locale["select"], GUI.MenuType.SelectBox, LineSelect.Locale["line_select"], { 0 }, 1, nil)
-		
-		--GUI.Subscribe(LineSelect.Identity, GUI.GameStates.OnLobbyLoad, LineSelect.WriteMessage)
 	end
+
 	if GameRules.GetGameState() == 1 and GUI.IsEnabled(LineSelect.Identity) then
-		Log.Write(Player.GetPlayerID(Players.GetLocal()))
 		if Player.GetPlayerID(Players.GetLocal()) >= 5 then
 			local line = GUI.Get(LineSelect.Identity .. "line_select_option", 1)[1]
 			if line == 5 then
@@ -71,6 +76,12 @@ function LineSelect.OnDraw()
 			Engine.ExecuteCommand("dota_select_starting_position " .. line)
 		else
 			Engine.ExecuteCommand("dota_select_starting_position " .. GUI.Get(LineSelect.Identity .. "line_select_option", 1)[1])
+		end
+
+		if GUI.Get(LineSelect.Identity .. "pickedHero", 1) == nil then return end
+
+		for k,v in pairs(GUI.Get(LineSelect.Identity .. "pickedHero", 1)) do
+			Engine.ExecuteCommand("possible_hero " .. string.sub(v, 15))
 		end
 	end
 end
